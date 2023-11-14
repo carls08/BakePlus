b3c53c00106216eb9253f57cf04540f45aa5c6c4
carls1720 <quintero.carlos.rocha@gmail.com>
2023-11-13 22:37:50 -0500

    final final

--- src/app/formulario-recetas/formulario-recetas.component.ts
+++ src/app/formulario-recetas/formulario-recetas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario-recetas',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario-recetas.component.css']
 })
 export class FormularioRecetasComponent {
+   prueba=[{"Ingrediente":"", "Cantidad":""}]
+    
+   inputTipo:string="";
+   inputCantidad:number = 0;
+   inputUnidad:string=""; 
 
+   agregar(){
+    this.prueba.push({"Ingrediente":this.inputTipo, "Cantidad":this.inputCantidad+" "+this.inputUnidad});
+    Swal.fire({
+    
+      icon: "success",
+      title: "Se ha agregado correctamemte",
+      showConfirmButton: false,
+      timer: 1500
+    });
+   }
+
+   
 }
