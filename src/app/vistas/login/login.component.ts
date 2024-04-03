4e8eaa442e42c2d269ef4ac4504c9d1c3d066218
Eder Melo Gonzalez <coordinadorsistemas@CABLEEXPRESS.COM>
2024-04-03 14:32:21 -0500

    V2.0.14 - fix en login

--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup,FormControl,Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { ApiService } from 'src/app/services/api/api.service';
 import { LoginI } from 'src/app/models/login.interface';
 import { ResponseI } from 'src/app/models/response.interface';
@@ -14,53 +14,87 @@ import Swal from 'sweetalert2';
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  usuarioClicked: boolean = false;
+  passClicked: boolean = false;
 
   constructor(
 
-    private router:Router, private api:ApiService, private fb:FormBuilder
-  ){
-    this.loginForm=this.fb.group({
-      usuario:['',Validators.required],
-      contraseña:['',Validators.required]
+    private router: Router, private api: ApiService, private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      contraseña: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  ngOnInit(): void {
+    this.checkLocalStorage();
+  }
+  checkLocalStorage() {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['home'])
+    }
+  }
+
+  onLogin(form: LoginI) {
+    console.log(form)
+    this.api.loginByEmail(form).subscribe(data => {
+      console.log(data)
+      let dataResponse: ResponseI = data;
+      if (dataResponse) {
+        localStorage.setItem("token", dataResponse.token);
+        localStorage.setItem("nombre_usuario", dataResponse.nombre_usuario);
+
+        this.router.navigate(['home'])
+        Swal.fire({
+          icon: "success",
+          title: "Has ingresado",
+          showConfirmButton: false,
+          timer: 1000
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Usuario o contraseña incorrecto",
+          footer: '<a href="">Intenta nuevamente</a>'
+        });
+
+      }
     })
   }
- ngOnInit():void {
-  this.checkLocalStorage();
- }
-  checkLocalStorage(){
-    if(localStorage.getItem('token')){
-      this.router.navigate(['home'])
+  // Método para validar el email
+  validateUsuario() {
+    if (!this.usuarioClicked) {
+      return false; // No mostrar error si no se ha hecho clic en el campo de apellido
     }
+    const emailControl = this.loginForm.get('usuario');
+    if (emailControl?.invalid && (emailControl?.dirty || emailControl?.touched)) {
+      return 'El correo no es valido!';
+    } else if (emailControl?.value.length <= 9) {
+      return 'Al menos 6 caracteres.'
+    }
+    return null; // El email es válido
   }
-
-  onLogin(form:LoginI){
-   console.log(form)
-   this.api.loginByEmail(form).subscribe(data =>{
-    console.log(data)
-    let dataResponse:ResponseI=data;
-    if(dataResponse){
-      localStorage.setItem("token",dataResponse.token);
-      localStorage.setItem("nombre_usuario",dataResponse.nombre_usuario);
-      
-      this.router.navigate(['home'])
-      Swal.fire({
-        icon: "success",
-        title: "Has ingresado",
-        showConfirmButton: false,
-        timer: 1000
-      });
-    }else{
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Usuario o contraseña incorrecto",
-        footer: '<a href="">Intenta nuevamente</a>'
-      });
-
+  // Método para validar pass
+  validatePass() {
+    if (!this.passClicked) {
+      return false; // No mostrar error si no se ha hecho clic en el campo de apellido
     }
-   })
-   
-    
-
+    const passControl = this.loginForm.get('contraseña');
+    if (passControl?.invalid && (passControl?.dirty || passControl?.touched)) {
+      return 'contraseña no es valido!';
+    } else if (passControl?.value.length <= 5) {
+      return 'Al menos 4 caracteres.'
+    }
+    return null; // El email es válido
+  }
+  onUsuarioClicked() {
+    this.usuarioClicked = true; // Marcar como true cuando se hace clic en el campo
+  }
+  onPassClicked() {
+    this.passClicked = true; // Marcar como true cuando se hace clic en el campo
+  }
+  isFormValid(): boolean {
+    return this.loginForm.valid; // Retorna true si el formulario es válido, de lo contrario retorna false
   }
 }
